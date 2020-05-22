42145ff53506a6b4911bbf22638a8a9e1d590385
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-05-22 15:24:55 -0400

    Add project files.

---
+++ RegistroCompletoAPP/BLL/PersonasBLL.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegistroCompletoAPP.Models;
+using RegistroCompletoAPP.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroCompletoAPP.BLL
+{
+    public class PersonasBLL
+    {
+        public static bool Guardar(Personas persona)
+        {
+            if (!Existe(persona.PersonaId))
+                return Insertar(persona);
+            else
+                return Modificar(persona);
+        }
+
+        private static bool Insertar(Personas persona)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Personas.Add(persona);
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Personas persona)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var persona = contexto.Personas.Find(id);
+
+                if(persona != null)
+                {
+                    contexto.Personas.Remove(persona);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Personas Buscar(int id)
+        {
+            Personas persona;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                persona = contexto.Personas.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return persona;
+        }
+
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Personas.Any(p => p.PersonaId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+    }
+}
---
+++ RegistroCompletoAPP/Models/Personas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using RegistroCompletoAPP.Models.Validaciones;
+
+namespace RegistroCompletoAPP.Models
+{
+    public class Personas
+    {
+        [Key]
+        [IdValidacion]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir un nombre")]
+        [NombresValidacion]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir un teléfono")]
+        [TelefonoValidacion]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir una cédula")]
+        [CedulaValidacion]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir una dirección")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir una fecha")]
+        [FechaNacimientoValidacion]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
